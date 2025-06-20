64062b48706cc4fe5af479b9cf6ad3507fc96316
brandaochico <brandao.chico24@gmail.com>
2025-06-20 15:16:34 -0300

    sprites - classes Player e Star

--- main.py
+++ main.py
@@ -2,54 +2,71 @@ import pygame
 from os.path import join
 from random import randint
 
+class Player(pygame.sprite.Sprite):
+    # construtor
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('images', 'player.png')).convert_alpha()
+        self.rect = self.image.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.direction = pygame.math.Vector2()
+        self.speed = 300
+
+    # update roda todos os frames
+    def update(self, dt):
+        # movimentação
+        keys = pygame.key.get_pressed()
+
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt
+
+        # "tiro"
+        recent_keys = pygame.key.get_just_pressed()
+
+        if recent_keys[pygame.K_SPACE]:
+            print('fire laser')
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, groups, surf):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center = (randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)))
+
+# setup geral
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Space Shooter')
 clock = pygame.Clock()
 
-# instanciando player
-player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
-player_rect = player_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = pygame.math.Vector2()
-player_speed = 300
+all_sprites = pygame.sprite.Group()
 
-# instanciando estrelas
+# instanciando estrelas no grupo all_sprites
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
-star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
+for i in range(20):
+    Star(all_sprites, star_surf)
+
+# instanciando player no grupo all_sprites
+player = Player(all_sprites)
 
 running = True
 
+# game loop
 while running:
     dt = clock.tick() / 1000
 
+    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    ''' movimentação do player '''
-    keys = pygame.key.get_pressed()
+    all_sprites.update(dt)
 
-    ## int(bool)
-    ## |--> int(True) = 1 | int(False) = 0
-    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
-    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
-
-    player_direction = player_direction.normalize() if player_direction else player_direction
-
-    player_rect.center += player_direction * player_speed * dt
-    ''''''
-
-    recent_keys = pygame.key.get_just_pressed()
-    if recent_keys[pygame.K_SPACE]:
-        print('fire laser')
-
+    # "desenhando" o jogo
     display.fill('darkgray')
 
-    for pos in star_positions:
-        display.blit(star_surf, pos)
-
-    display.blit(player_surf, player_rect)
+    all_sprites.draw(display)
 
     pygame.display.update()
 
